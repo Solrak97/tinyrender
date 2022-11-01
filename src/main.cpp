35f063a68ec54ff99d32ca2a1aa987eef4e7cd51
solrak <luiscarlosquesada@gmail.com>
2022-11-01 00:31:56 -0600

    Lines! whats going on???

---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+#include "tgaimage.hpp"
+#include <cstdlib>
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+{
+
+    bool steep = false;
+    if (std::abs(x0 - x1) < std::abs(y0 - y1))
+    {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1)
+    {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int derror2 = std::abs(dy) * 2;
+    int error2 = 0;
+    int y = y0;
+    for (int x = x0; x <= x1; x++)
+    {
+        if (steep)
+        {
+            image.set(y, x, color);
+        }
+        else
+        {
+            image.set(x, y, color);
+        }
+        error2 += derror2;
+        if (error2 > dx)
+        {
+            y += (y1 > y0 ? 1 : -1);
+            error2 -= dx * 2;
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int imgWidth = 500;
+    int imgHeight = 500;
+
+    TGAImage image(imgHeight, imgWidth, TGAImage::RGB);
+
+    line(0, 0, 200, 200, image, red);
+    line(0, 200, 500, 200, image, red);
+    line(0, 400, 500, 0, image, red);
+    image.flip_vertically();
+
+    image.write_tga_file("output.tga");
+    return 0;
+}
