efad2882c7c31d080fda2e746cb41b4add296211
solrak <luiscarlosquesada@gmail.com>
2022-11-02 01:28:53 -0600

    barycentric coords are not that hard!

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # tinyrender
+
+I've been working in graphics for a while using game engines, as I wanted a deeper understanding of what was going on, I decided to try to build something from scratch.
+
+The tinyrender is a project  based on the material from https://github.com/ssloy/tinyrenderer/wiki, it's really helpful, just some basic notions of linear algebra and C++ are enough to start building something interesting.
+
+---
+
+### What I've learned so far
+
+* Sometimes there is a +1 on the code, it seemed random at first but its quite easy to understand after reading the .obj vectors.
+  
+     ```
+     v -0.201076 -0.720822 -0.633205 
+     ```
+
+    Vector coordinates are defined as a float in the range [-1, 1], those points must be scaled to fit on the screen or image, but the target only deals in positive num,bers, so before scaling the point it must be transformed to a positive scale, adding +1 to the original point will 'center' the point to the range [0, 2].
+    
+    ```c++
+    model.vert(face[j]).x + 1) * width / 2;
+    ```
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 {
@@ -58,6 +59,9 @@ void mesh(const char *filename, TGAImage &image)
         std::vector<int> face = model.face(i);
         // this'll get the 2 vertices to draw a line
         // but why does it need the +1?
+
+        //On the .obj file, space is defined between -1 and 1
+        //As we deal only on positive integers the +1 centers the data
         for (int j = 0; j < 3; j++)
         {
             Vec3f v0 = model.vert(face[j]);
@@ -74,75 +78,45 @@ void mesh(const char *filename, TGAImage &image)
     }
 }
 
-/* Totally experimental and trashy code, do not touch by the love of God! */
-std::vector<Vec2i> linePoints(int x0, int y0, int x1, int y1)
+Vec3f barycentric(Vec2i *tri, Vec2i P)
 {
-    std::vector<Vec2i> points = std::vector<Vec2i>();
+    //Remember ^ as cross product operator!
+    // u = (AC0, AB0, PA0) X (AC1, AB1, PA1)
+    // => u orthogonal to both vectors
+    Vec3f u = Vec3f{tri[2].x - tri[0].x, tri[1].x - tri[0].x, tri[0].x - P.x} ^
+              Vec3f { tri[2].x - tri[0].x, tri[1].x - tri[0].x, tri[0].x - P.x };
+    
 
-    bool steep = false;
-    if (std::abs(x0 - x1) < std::abs(y0 - y1))
-    {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
-        steep = true;
-    }
-    if (x0 > x1)
-    {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
-    }
-    int dx = x1 - x0;
-    int dy = y1 - y0;
-    int derror2 = std::abs(dy) * 2;
-    int error2 = 0;
-    int y = y0;
-    for (int x = x0; x <= x1; x++)
-    {
-        if (steep)
-        {
-            points.push_back(Vec2i{y, x});
-        }
-        else
-        {
-            points.push_back(Vec2i{x, y});
-        }
-        error2 += derror2;
-        if (error2 > dx)
-        {
-            y += (y1 > y0 ? 1 : -1);
-            error2 -= dx * 2;
-        }
-    }
-
-    return points;
 }
 
 void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
 {
-    // Bubble sort goes brrr
-    Vec2i tpoints[3] = {t0, t1, t2};
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            if (tpoints[j].y > tpoints[j + 1].y)
-            {
-                Vec2i t = tpoints[j + 1];
-                tpoints[j + 1] = tpoints[j];
-                tpoints[j] = t;
-            }
-        }
-    }
+    if (t0.y == t1.y && t0.y == t2.y)
+        return; // I dont care about degenerate triangles
+    // that explains kinda a lot
 
-    std::vector<Vec2i> A = linePoints(tpoints[0].x, tpoints[0].y, tpoints[2].x, tpoints[2].y);
-    std::vector<Vec2i> B1 = linePoints(tpoints[0].x, tpoints[0].y, tpoints[1].x, tpoints[1].y);
-    std::vector<Vec2i> B2 = linePoints(tpoints[1].x, tpoints[1].y, tpoints[2].x, tpoints[2].y);
-
-    for (int i = 0; i < A.size(); i++)
+    if (t0.y > t1.y)
+        std::swap(t0, t1);
+    if (t0.y > t2.y)
+        std::swap(t0, t2);
+    if (t1.y > t2.y)
+        std::swap(t1, t2);
+    int total_height = t2.y - t0.y;
+    for (int i = 0; i < total_height; i++)
     {
-        for (int j = 0; j < B1.size(); j++)
+        bool second_half = i > t1.y - t0.y || t1.y == t0.y;
+        int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+        float alpha = (float)i / total_height;
+        float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height; // be careful: with above conditions no division by zero here
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+
+        // Axis swap, always draw left to right
+        if (A.x > B.x)
+            std::swap(A, B);
+        for (int j = A.x; j <= B.x; j++)
         {
-            line(A[i].x, A[i].y, B1[j].x, B1[j].y, image, color);
+            image.set(j, t0.y + i, color); // attention, due to int casts t0.y+i != A.y
         }
     }
 }
@@ -161,21 +135,19 @@ void filledMesh(const char *filename, TGAImage &image)
 
         for (int j = 0; j < 3; j++)
         {
-            pts[j] = Vec2i{(model.vert(face[j]).x + 1) * width / 2,
-                           (model.vert(face[j]).y + 1) * height / 2};
+            pts[j] = Vec2i{
+                (int)((model.vert(face[j]).x + 1) * width / 2),
+                (int)((model.vert(face[j]).y + 1) * height / 2)};
         }
 
-        std::cout<<"triangle"<<std::endl;
         triangle(pts[0], pts[1], pts[2], image, white);
-        std::cout<<"end triangle"<<std::endl;
     }
 }
-
-/* End of totally trashy and experimental code */
 
 int main(int argc, char **argv)
 {
     TGAImage image(500, 500, TGAImage::RGB);
+    // mesh("african_head.obj", image);
     filledMesh("african_head.obj", image);
     image.write_tga_file("output.tga");
     return 0;
